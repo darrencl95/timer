871ad9dcce62facbdd5eb480aed0ac68b59db752
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-03-28 22:17:02 -0700

    Initial Commit

---
+++ Timer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Timer
+//
+//  Created by Kazuyoshi Kato on 3/28/18.
+//  Copyright © 2018 Kazuyoshi Kato. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
